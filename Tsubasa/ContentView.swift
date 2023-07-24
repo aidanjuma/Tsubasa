3ec3a7a6eb1a7acea075b79abc1df998215b7e7b
Aidan Juma <aidan@aidanjuma.dev>
2023-07-24 23:20:35 +0100

    🎉 initial commit - project generation

---
+++ Tsubasa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tsubasa
+//
+//  Created by Aidan Juma on 24/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tsubasa/TsubasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TsubasaApp.swift
+//  Tsubasa
+//
+//  Created by Aidan Juma on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TsubasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
